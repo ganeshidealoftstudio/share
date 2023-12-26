75c51e5fb97c19287cb8fcf3b048b314f56a18b6
ganesh <ganesh@idealoftstudio.com>
2023-12-26 19:40:24 +0530

    link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import "./App.css";
 
 function App() {
+  const handleonsubmit = async () => {
+    const response = await fetch("https://picsum.photos/200/300");
+    // here image is url/location of image
+    // const blob = await response.blob();
+    // const file = new file([blob], "share.jpg", { type: blob.type });
+    // console.log(file);
+    console.log("here");
+    if (navigator.share) {
+      await navigator
+        .share({
+          title: "title",
+          text: "your text",
+          url: "url to share",
+          files: "https://picsum.photos/200/300",
+        })
+        .then(() => console.log("successful share"))
+        .catch((error) => console.log("error in sharing", error));
+    } else {
+      console.log(`system does not support sharing files.`);
+    }
+  };
   return (
     <div className="App">
       <h1>ganesh</h1>
-      <h2>click here</h2>
+      <h2 onClick={handleonsubmit}>click here</h2>
       <img src="https://picsum.photos/200/300" alt="hre" />
     </div>
   );
