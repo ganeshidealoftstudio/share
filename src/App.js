30a7fe6ab0a891b0c08ef0e55d284747db8db2db
ganesh <ganesh@idealoftstudio.com>
2023-12-26 19:33:30 +0530

    image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>ganesh</h1>
+      <h2>click here</h2>
+      <img src="https://picsum.photos/200/300" alt="hre" />
     </div>
   );
 }
